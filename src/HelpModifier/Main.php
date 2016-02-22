280f97e4c995c9856bdce83a154a5be9ebda578d
applqpak <applqpak@gmail.com>
2016-02-23 09:14:18 +1100

    Update Main.php

--- src/HelpModifier/Main.php
+++ src/HelpModifier/Main.php
@@ -133,3 +133,27 @@
           $player->sendMessage($page_four_messages["message_9"]);
 
           $player->sendMessage($page_four_messages["message_10"]);
+
+        } else if($command[1] === "5" or $command[1] === "five") {
+
+          $page_five_messages = $this->cfg->get("page_5");
+
+          $player->sendMessage($page_five_messages["message_1"]);
+
+          $player->sendMessage($page_five_messages["message_2"]);
+
+          $player->sendMessage($page_five_messages["message_3"]);
+
+          $player->sendMessage($page_five_messages["message_4"]);
+
+          $player->sendMessage($page_five_messages["message_5"]);
+
+          $player->sendMessage($page_five_messages["message_6"]);
+
+          $player->sendMessage($page_five_messages["message_7"]);
+
+          $player->sendMessage($page_five_messages["message_8"]);
+
+          $player->sendMessage($page_five_messages["message_9"]);
+
+          $player->sendMessage($page_five_messages["message_10"]);
